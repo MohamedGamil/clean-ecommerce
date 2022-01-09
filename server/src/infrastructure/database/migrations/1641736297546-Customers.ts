53a7bb7f74ab0de2e3c784b3e6125d5f78f82a99
Mohamed Gamil <gemy.dev@gmail.com>
2022-01-09 15:54:11 +0200

    MySQL Compatability + Make migration command shorthand + Customers and Orders Migrations

---
+++ server/src/infrastructure/database/migrations/1641736297546-Customers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Customers1641736297546 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
